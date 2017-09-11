7f5415858a36be03034af9007aed1a2d76b9a5d9
steven <slamslamslam12411@gmail.com>
2017-09-12 07:37:48 +0800

    9-12

--- app/src/main/java/com/example/user/numlogin/MainActivity.java
+++ app/src/main/java/com/example/user/numlogin/MainActivity.java
@@ -9,16 +9,20 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     Button logBtn;
     FirebaseAuth auth;
+    FirebaseUser user;
+    FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +30,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         logBtn = (Button) findViewById(R.id.LogBtn);
 
+
         //when push button logBtn
         logBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                login();
 
             }
 
@@ -71,12 +77,72 @@ public class MainActivity extends AppCompatActivity {
                                         String message = task.isComplete() ? "Success" : "Failed";
                                         new AlertDialog.Builder(MainActivity.this)
                                                 .setMessage(message)
-                                                .setPositiveButton("OK",null)
+                                                .setPositiveButton("OK", null)
                                                 .show();
+
                                     }
                                 });
 
             }
+
+            private void sendVerify() {
+                final FirebaseUser user = auth.getCurrentUser();
+                user.sendEmailVerification()
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(MainActivity.this, "驗證信傳送成功", Toast.LENGTH_SHORT).show();
+                                } else{
+                                    Toast.makeText(MainActivity.this, "Email 驗證:"+task.getException().toString(), Toast.LENGTH_SHORT).show();
+                                }
+                                new AlertDialog.Builder(MainActivity.this)
+                                        .setMessage("請驗證Email")
+                                        .setTitle("Email驗證")
+                                        .setPositiveButton("確認",new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which){
+                                                if(user.isEmailVerified()){
+                                                    Toast.makeText(MainActivity.this, "驗證成功", Toast.LENGTH_SHORT).show();
+                                                } else {
+                                                    Toast.makeText(MainActivity.this, "尚未驗證", Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        })
+                                        .show();
+                            }
+                        });
+            }
         });
+        auth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.d("onAuthStateChanged", "登入:"+user.getUid());
+                } else {
+                    Log.d("onAuthStateChanged", "已登出");
+                }
+
+            }
+
+
+        };
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        auth.removeAuthStateListener(authStateListener);
+    }
+
 }
